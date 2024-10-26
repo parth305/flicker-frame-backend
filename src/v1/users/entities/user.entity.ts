7a873bdf799979bc92d0e146828ba62ef3682d8d
tbm02 <tarun989898@gmail.com>
2024-10-26 21:01:21 +0530

    chore : dropped user role field from user entity

--- src/v1/users/entities/user.entity.ts
+++ src/v1/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
@@ -7,8 +7,6 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-import { EUsersRole } from '@/src/v1/users/types/user.type';
-
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -16,25 +14,20 @@ export class User {
 
   @Column()
   @MinLength(2, {
-    message: 'userName must be longer than or equal to 2 characters',
+    message: 'Username must be longer than or equal to 2 characters',
   })
   userName: string;
 
   @Column({ unique: true, nullable: false, update: false }) // default nullable is false
-  @IsEmail(undefined, { message: 'userEmail must be an email' })
+  @IsEmail(undefined, { message: 'Email must be an email' })
   userEmail: string;
 
   @Column()
   @MinLength(5, {
     message: 'userPassword must be longer than or equal to 5 characters',
   })
-  @IsNotEmpty({ message: 'userPassword should not be empty' })
+  @IsNotEmpty({ message: 'Password should not be empty' })
   userPassword: string;
-
-  @Column({ type: 'enum', enum: EUsersRole, default: EUsersRole.USER })
-  @IsNotEmpty({ message: 'userRole should not be empty' })
-  @IsEnum(EUsersRole, { message: 'Invalid userRole' })
-  userRole: EUsersRole;
 
   @CreateDateColumn()
   createdAt: Date;
