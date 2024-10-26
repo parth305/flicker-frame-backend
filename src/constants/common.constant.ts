d8904198dfd367d055c16266f5e6f7acd731a9e0
tbm02 <tarun989898@gmail.com>
2024-10-27 02:38:56 +0530

    feat : added constant for error messages.

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -3,4 +3,9 @@ export const CONSTANTS = {
     USER_FIRST_NAME_MAX_LENGTH: 30,
     USER_LAST_NAME_MAX_LENGTH: 30,
   },
+  ERROR_MESSAGE: {
+    TOKEN_MISSING: 'Access Token Missing',
+    INVALID_TOKEN: 'Invalid token',
+    USER_ALREADY_LOGGED_OUT: 'User Already token',
+  },
 };
