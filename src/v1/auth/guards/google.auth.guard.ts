bfb6a6d13e258ab040b3d97e70c1fe989006a78b
tbm02 <tarun989898@gmail.com>
2024-11-09 04:23:44 +0530

    fix: Fixed Username Generation Logic.

---
+++ src/v1/auth/guards/google.auth.guard.ts
@@ -0,0 +1,9 @@
+// import { AuthGuard } from '@nestjs/passport';
+
+// export class GoogleAuthGuard extends AuthGuard('google'){
+//     constructor(params){
+//         super(params);
+
+//     }
+
+// }
